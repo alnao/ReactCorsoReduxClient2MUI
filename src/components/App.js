9220c7b1c9e80f4c27e7dedbcf1a353e05c9a04a
Alberto Nao <alnao@alnao.it>
2022-01-22 16:57:23 +0100

    componenti per visualizzazione esercizi

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,57 @@
+import { Grid, List, ListItem, ListItemText, Paper, Typography } from "@material-ui/core";
+import { Component, Fragment } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
-function App() {
-  return (
-    <div className="App">
+const style={
+  Paper: {padding:10,margin:10}
+}
+export const muscoli = ['spalle','addominali','gambe'];
+export const esercizi = [
+  {id:'flessioni', muscoli:'spalle'},
+  {id:'pesi', muscoli:'spalle'},
+  {id:'piegamenti', muscoli:'addominali'},
+  {id:'pushp', muscoli:'addominali'},
+  {id:'corsa', muscoli:'gambe'},
+];
+
+class App extends Component {
+	state={esercizi}
+	getEserciziPerGruppo = () => {
+		return Object.entries(
+    this.state.esercizi.reduce((esercizi,esercizio) => {
+			const {muscoli} = esercizio;
+			esercizi[muscoli]=esercizi[muscoli] ? [...esercizi[muscoli],esercizio] : [esercizio]
+			return esercizi;
+		},{}));
+	}
+  render(){return (
+    <div className="App" >
       <Header></Header>
-      <p>React Redux Client 2 with MUI</p>
-      <Footer></Footer>
+      <Grid container spacing={10} >
+        <Grid item xs={5}  >
+          <Paper elevation={1} style={style.Paper}>
+              {this.state.esercizi.map (({id,muscoli}) =>
+                <Fragment>
+                  <Typography variant="h4">{muscoli}</Typography>
+                  <List component="nav">
+                    {esercizi.map(({id,muscoli}) => (
+                      <ListItem button><ListItemText>{id}</ListItemText></ListItem>
+                    ))}
+                  </List>
+                </Fragment>
+              )}
+          </Paper>
+        </Grid>
+        <Grid item xs={5} >
+          <Paper elevation={1} style={style.Paper}>
+              Card dx</Paper>
+        </Grid>
+      </Grid>
+
+      <Footer muscoli={muscoli}></Footer>
     </div>
-  );
+  );}
 }
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
+import { Paper, Tab, Tabs } from '@material-ui/core';
 import React,{Component} from 'react';
+
 class Footer extends Component{
-    render(){
-        return (
-            <div>
-                <p>Footer</p>
-            </div>
-        );
-    }
+    state = {value:'addominali'};
+    hchange = (e,value) => {  this.setState({value:value}); } //no hchange(e,v){...}
+    render(){ return (
+        <Paper >
+            <Tabs value={this.state.value}
+            onChange={this.hchange}
+            indicatorColor="primary"
+            textColor="primary"
+            contered="true">
+                {this.props.muscoli.map (gruppo =>
+                    <Tab key={gruppo} label={gruppo} value={gruppo}/>
+                )}
+            </Tabs>
+        </Paper>
+    );}
 }
 export default Footer;
